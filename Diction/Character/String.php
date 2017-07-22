7fba20bed3a48c52add9e19f849689056876f8d4
proson <proson@163.com>
2017-07-22 15:39:32 +0800

    init

---
+++ Diction/Character/String.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MistPack\Diction\Character;
+
+/**
+ *
+ * 文字处理类
+ * @author mark<mk9007@163.com>
+ */
+class String {
+
+    /**
+     * 对提供的文字内容进行占位符操作
+     * 默认对系统中间的文字串进行占位符操作
+     * @author mark<mk9007@163.com>
+     * @param String $str 
+     * @return String 
+     */
+    public static function placeholder($str) {
+        if (empty($str))
+            return;
+        
+    }
+
+}

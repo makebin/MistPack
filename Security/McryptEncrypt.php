9a160611e4c6c8c370b23a6774d705c9107fed31
proson <proson@163.com>
2017-07-27 14:18:59 +0800

    add security

---
+++ Security/McryptEncrypt.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of McryptEncrypt
+ * @filename McryptEncrypt.php 
+ * @encoding UTF-8 
+ * @author mark <mk9007@163.com>
+ * @datetime 2017-7-27 14:13:26
+ */
+class McryptEncrypt {
+
+    public function __construct() {
+        
+    }
+
+    public function mcryptEncode() {
+        
+    }
+
+    public function mcryptDecode() {
+        
+    }
+
+}
